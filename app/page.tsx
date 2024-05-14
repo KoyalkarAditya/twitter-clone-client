6ccd5112e6a028ee4ab3a497d9b80631f71e259b
KoyalkarAditya <koyalkaraditya123@gmail.com>
2024-05-14 14:27:36 +0530

    created feed card

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,9 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import { HiOutlineMail } from "react-icons/hi";
 import { PiBookmarkSimple } from "react-icons/pi";
 import { CiUser } from "react-icons/ci";
-
+import { FeedCard } from "@/components/FeedCard";
+import { CiCircleMore } from "react-icons/ci";
+import { BiMoney } from "react-icons/bi";
 export default function Home() {
   interface TwitterSidebarButton {
     title: string;
@@ -36,11 +38,19 @@ export default function Home() {
       title: "Profile",
       icon: <CiUser />,
     },
+    {
+      title: "Blue",
+      icon: <BiMoney />,
+    },
+    {
+      title: "More",
+      icon: <CiCircleMore />,
+    },
   ];
   return (
     <div>
-      <div className=" grid grid-cols-12 h-screen w-screen px-56">
-        <div className=" col-span-3  justify-start pt-8  pr-4">
+      <div className=" grid grid-cols-12 h-screen w-screen px-40">
+        <div className=" col-span-3  justify-start   pr-4">
           <div className="h-fit cursor-pointer w-fit ml-2 text-3xl  hover:bg-gray-800 transition-all rounded-full p-2">
             <FaXTwitter />
           </div>
@@ -58,12 +68,17 @@ export default function Home() {
                 </li>
               ))}
             </ul>
-            <button className=" w-full font-semibold rounded-full p-3  mt-4 bg-[#1d9bf0] hover:bg-[#1a8cd8]">
+            <button className=" w-[90%] font-semibold rounded-full p-3  mt-4 bg-[#1d9bf0] hover:bg-[#1a8cd8]">
               Post
             </button>
           </div>
         </div>
-        <div className=" col-span-6  border-gray-600 border-r-[1px] border-l-[1px]"></div>
+        <div className=" col-span-6  border-gray-600 border-r-[1px] border-l-[1px]">
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+        </div>
         <div className=" col-span-3"></div>
       </div>
     </div>
---
+++ components/FeedCard/index.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import { FaRegComment } from "react-icons/fa";
+import { FaRetweet } from "react-icons/fa6";
+import { CiHeart } from "react-icons/ci";
+import { GoUpload } from "react-icons/go";
+import { CiBookmark } from "react-icons/ci";
+import { Quicksand } from "next/font/google";
+const quicksand = Quicksand({
+  subsets: ["latin"],
+});
+export const FeedCard: React.FC = () => {
+  return (
+    <div>
+      <div className=" grid grid-cols-12 p-5 gap-2 hover:bg-gray-900 hover:bg-opacity-40 transition-all cursor-pointer border-gray-900 border-b-[1px] border-t-[1px]">
+        <div className=" col-span-1">
+          <Image
+            width={50}
+            height={50}
+            src={
+              "https://pbs.twimg.com/profile_images/1788655525196070913/UFnOzVKB_400x400.jpg"
+            }
+            alt="profile-pic"
+            className=" rounded-full"
+          />
+        </div>
+        <div className="col-span-11">
+          <h5 className="">Koyalkar Aditya</h5>
+          <p>
+            This i the content contentThis i the content contentThis i the
+            content contentThis i the content contentThis i the content
+            contentThis i the content content
+          </p>
+
+          <div
+            className={`${quicksand.className} flex w-[90%] justify-between mt-5 text-lg text-slate-500 font-semibold `}
+          >
+            <div>
+              <FaRegComment />
+            </div>
+            <div>
+              <FaRetweet />
+            </div>
+            <div>
+              <CiHeart />
+            </div>
+            <div>
+              <CiBookmark />
+            </div>
+            <div>
+              <GoUpload />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
